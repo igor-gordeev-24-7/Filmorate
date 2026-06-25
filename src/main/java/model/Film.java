c254612b05af2136ed118fdeab1df6bdf9d7b756
igor-gordeev-24-7 <igor.gordeev.24.7@gmail.com>
2026-06-25 15:05:29 +0300

    feat(Film.java): создан класс Film, добавлены модели данных

---
+++ src/main/java/model/Film.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Film {
+    private int id;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private double duration;
+}

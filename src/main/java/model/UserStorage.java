1894928df8154d23b0474905aa728fbf566b17fe
igor-gordeev-24-7 <igor.gordeev.24.7@gmail.com>
2026-06-25 16:05:55 +0300

    refactor(UserStorage.java) создан класс UserStorage, добавлены методы

---
+++ src/main/java/model/UserStorage.java
@@ -0,0 +1,36 @@
+package model;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Component
+public class UserStorage {
+    private final Map<Integer, User> users = new HashMap<>();
+    private int currentId = 1;
+
+    public User addUser(User user) {
+        user.setId(currentId++);
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    public Collection<User> getAllUsers() {
+        return users.values();
+    }
+
+    public Optional<User> getUserById(int id) {
+        return Optional.ofNullable(users.get(id));
+    }
+
+    public User updateUser(User user) {
+        if (users.containsKey(user.getId())) {
+            users.put(user.getId(), user);
+            return user;
+        }
+        throw new RuntimeException("Пользователь с id " + user.getId() + " не найден");
+    }
+}

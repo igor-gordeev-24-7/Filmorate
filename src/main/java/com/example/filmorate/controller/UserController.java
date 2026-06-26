afecac1139968c6b6c91a61baf508d6c72a3be7f
igor-gordeev-24-7 <igor.gordeev.24.7@gmail.com>
2026-06-26 16:07:37 +0300

    feat(UserController.java): добавлен метод updateUser

--- src/main/java/com/example/filmorate/controller/UserController.java
+++ src/main/java/com/example/filmorate/controller/UserController.java
@@ -2,10 +2,12 @@ package com.example.filmorate.controller;
 
 import com.example.filmorate.model.User;
 import com.example.filmorate.repository.UserRepository;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -33,8 +35,20 @@ public class UserController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @PutMapping
-    public void updateUser(User user) {
-        userRepository.updateUser(user);
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(
+            @PathVariable int id,
+            @Valid @RequestBody User user) {
+
+        user.setId(id);
+
+        Optional<User> existingUser = userRepository.getUserById(id);
+
+        if (existingUser.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        User updatedUser = userRepository.updateUser(user);
+        return ResponseEntity.ok(updatedUser);
     }
 }

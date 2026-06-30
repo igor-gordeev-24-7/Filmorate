6df356ecbbd5de2d42f1a597bf6453b676a73e0d
igor-gordeev-24-7 <igor.gordeev.24.7@gmail.com>
2026-06-30 13:49:39 +0300

    feat(Film.java): добавлены аннотации

--- src/main/java/com/example/filmorate/model/Film.java
+++ src/main/java/com/example/filmorate/model/Film.java
@@ -19,12 +19,17 @@ import java.time.LocalDate;
 public class Film {
     private Integer id;
     @NotBlank(message = "Название фильма не может быть пустым")
-    @Size(min = 1, max = 100, message = "Название фильма должно быть от 1 до 100 символов")
+    @Size(min = 1, max = 30, message = "Название фильма должно быть от 1 до 30 символов")
     private String name;
-    @Size(max = 50, min = 5)
+    @NotBlank(message = "Описание фильма не может быть пустым")
+    @Size(min = 5 ,max = 50, message = "Название фильма должно быть от 1 до 100 символов")
     private String description;
+    @NotNull
+    @ValidReleaseDate
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate releaseDate;
-    @Positive
+    @Positive(message = "Длительность должна быть положительным числом")
+    @DecimalMin(value = "1.0", message = "Минимальная длительность - 1 минута")
+    @DecimalMax(value = "600.0", message = "Максимальная длительность - 600 минут")
     private Double duration;
 }

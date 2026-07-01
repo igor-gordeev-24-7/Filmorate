444ca553c420b71d9dac424fe7ceaf50b8c02c41
igor-gordeev-24-7 <igor.gordeev.24.7@gmail.com>
2026-07-01 21:48:07 +0300

    refactor (Film.java): добавлены пробелы для визуального разделения

--- src/main/java/com/example/filmorate/model/Film.java
+++ src/main/java/com/example/filmorate/model/Film.java
@@ -15,17 +15,22 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Film {
+
     private Integer id;
+
     @NotBlank(message = "Название фильма не может быть пустым")
     @Size(min = 1, max = 30, message = "Название фильма должно быть от 1 до 30 символов")
     private String name;
+
     @NotBlank(message = "Описание фильма не может быть пустым")
     @Size(min = 5 ,max = 50, message = "Название фильма должно быть от 1 до 100 символов")
     private String description;
+
     @NotNull
     @ValidReleaseDate(minDate = "1800-12-12")
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate releaseDate;
+
     @Positive(message = "Длительность должна быть положительным числом")
     @DecimalMin(value = "1.0", message = "Минимальная длительность - 1 минута")
     @DecimalMax(value = "600.0", message = "Максимальная длительность - 600 минут")

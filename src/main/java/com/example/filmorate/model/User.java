413a8877250c0c06fc3ac6adcd295d1959c35582
igor-gordeev-24-7 <igor.gordeev.24.7@gmail.com>
2026-07-03 16:07:11 +0300

    feat (User.java): добавлены аннотации к полям класса

--- src/main/java/com/example/filmorate/model/User.java
+++ src/main/java/com/example/filmorate/model/User.java
@@ -1,16 +1,30 @@
 package com.example.filmorate.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Data;
+import jakarta.validation.constraints.*;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private int id;
+
+    @NotBlank(message = "Email не может быть пустым")
+    @Email(message = "Некорректный формат email")
     private String email;
+
+    @NotBlank(message = "Логин не может быть пустым")
     private String login;
+
     private String name;
+
+    @NotNull(message = "Дата рождения не может быть пустой")
+    @Past(message = "Дата рождения должна быть в прошлом")
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
 }

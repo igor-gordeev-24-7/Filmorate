0b9d2f8dff23d991688fe93c75cc573b90e6103f
igor-gordeev-24-7 <igor.gordeev.24.7@gmail.com>
2026-07-03 21:30:00 +0300

    style (User.java): убраны аннотации полей класса

--- src/main/java/com/example/filmorate/model/User.java
+++ src/main/java/com/example/filmorate/model/User.java
@@ -1,30 +1,27 @@
 package com.example.filmorate.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
-@Data
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
-    private int id;
 
-    @NotBlank(message = "Email не может быть пустым")
-    @Email(message = "Некорректный формат email")
+    private Integer id;
+
     private String email;
 
-    @NotBlank(message = "Логин не может быть пустым")
     private String login;
 
     private String name;
 
-    @NotNull(message = "Дата рождения не может быть пустой")
-    @Past(message = "Дата рождения должна быть в прошлом")
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
 }

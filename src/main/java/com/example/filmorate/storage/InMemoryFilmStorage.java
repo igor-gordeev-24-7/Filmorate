09b3dadf4e374b4c99b85306759f2b0bff63c45f
igor-gordeev-24-7 <igor.gordeev.24.7@gmail.com>
2026-07-03 15:08:12 +0300

    feat (InMemoryFilmStorage.java): создан класс, и определены методы добавления, удаления и модификации объектов

---
+++ src/main/java/com/example/filmorate/storage/InMemoryFilmStorage.java
@@ -0,0 +1,75 @@
+package com.example.filmorate.storage;
+
+import com.example.filmorate.exeption.EntityNotFoundException;
+import com.example.filmorate.model.Film;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class InMemoryFilmStorage implements FilmStorage {
+    private final Map<Integer, Film> films = new HashMap<>();
+    private int currentId = 1;
+
+    @Override
+    public Film addFilm(Film film) {
+        film.setId(currentId++);
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @Override
+    public List<Film> getAllFilms() {
+        if (!films.isEmpty()) {
+            return new ArrayList<>(films.values());
+        }
+        throw new EntityNotFoundException(
+                "Фильмы не найдены",
+                List.of("Cписок пуст"));
+    }
+
+    @Override
+    public Film getFilmById(int id) {
+        if (films.containsKey(id)) {
+            return films.get(id);
+        }
+        throw new EntityNotFoundException(
+                "Фильм с id " + id + " не найден",
+                List.of("Проверьте корректный ли id"));
+    }
+
+    @Override
+    public Film updateFilm(int id, Film film) {
+        if (films.containsKey(id)){
+            film.setId(id);
+            films.put(id, film);
+            return film;
+        }
+        throw new EntityNotFoundException(
+                "Фильм с id " + id + " не найден",
+                List.of("Проверьте корректный ли id"));
+    }
+
+    @Override
+    public void deleteFilms() {
+        if (films.isEmpty()) {
+            throw new EntityNotFoundException(
+                    "Записи не найдены",
+                    List.of("Удаление невозможно"));
+        }
+        films.clear();
+    }
+
+    @Override
+    public Film deleteFilmById(int id) {
+        if (films.containsKey(id)) {
+            return films.remove(id);
+        }
+        throw new EntityNotFoundException(
+                "Фильм с id " + id + " не найден",
+                List.of("Проверьте корректный ли id"));
+    }
+}

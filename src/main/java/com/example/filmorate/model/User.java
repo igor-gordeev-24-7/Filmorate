de34938284e48326bdb35f0bfdab23c96142cba2
igor-gordeev-24-7 <igor.gordeev.24.7@gmail.com>
2026-07-03 21:56:06 +0300

    refactor (User.java): добавлены аннотации

--- src/main/java/com/example/filmorate/model/User.java
+++ src/main/java/com/example/filmorate/model/User.java
@@ -1,6 +1,10 @@
 package com.example.filmorate.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,12 +20,18 @@ public class User {
 
     private Integer id;
 
+    @NotBlank(message = "Email не может быть пустым")
+    @Email(message = "Некорректный формат email")
     private String email;
 
+    @NotBlank(message = "Логин не может быть пустым")
     private String login;
 
+    @NotBlank(message = "Имя не может быть пустым")
     private String name;
 
+    @NotNull(message = "Дата рождения не может быть пустой")
+    @Past(message = "Дата рождения должна быть в прошлом")
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
 }

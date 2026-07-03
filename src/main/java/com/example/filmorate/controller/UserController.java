c80f12da5095edda10a6601457261ce070129f95
igor-gordeev-24-7 <igor.gordeev.24.7@gmail.com>
2026-07-03 16:08:24 +0300

    feat (UserController.java): UserRepository заменен на inMemoryUserStorage

--- src/main/java/com/example/filmorate/controller/UserController.java
+++ src/main/java/com/example/filmorate/controller/UserController.java
@@ -1,44 +1,55 @@
 package com.example.filmorate.controller;
 
 import com.example.filmorate.model.User;
-import com.example.filmorate.repository.UserRepository;
+import com.example.filmorate.storage.InMemoryUserStorage;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
-import java.util.Optional;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private final UserRepository userRepository;
+    private final InMemoryUserStorage inMemoryUserStorage;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserController(InMemoryUserStorage inMemoryUserStorage) {
+        this.inMemoryUserStorage = inMemoryUserStorage;
     }
 
     @PostMapping
     public ResponseEntity<User> addUser(@RequestBody User user) {
-        User savedUser = userRepository.addUser(user);
+        User savedUser = inMemoryUserStorage.addUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
     }
 
     @GetMapping
     public Collection<User> getAllUsers() {
-        return userRepository.getAllUsers();
+        return inMemoryUserStorage.getAllUsers();
     }
 
     @GetMapping("/{id}")
     public User getUserById(@PathVariable int id) {
-        return userRepository.getUserById(id);
+        return inMemoryUserStorage.getUserById(id);
     }
 
     @PutMapping("/{id}")
     public User updateUser(
             @PathVariable int id,
             @Valid @RequestBody User user) {
-        return userRepository.updateUser(id, user);
+        return inMemoryUserStorage.updateUser(id, user);
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<Map<String, String>> deleteUsers() {
+        inMemoryUserStorage.deleteUsers();
+        return ResponseEntity.ok(Map.of("message", "Все записи успешно удалены"));
+    }
+
+    @DeleteMapping("/{id}")
+    public User deleteUserById(@PathVariable int id) {
+        return inMemoryUserStorage.deleteUserById(id);
     }
 }

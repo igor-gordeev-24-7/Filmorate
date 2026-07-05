3bb853883255c5db20b2dc6351c3afdb30c0af6b
igor-gordeev-24-7 <igor.gordeev.24.7@gmail.com>
2026-07-05 12:45:31 +0300

    refactor (UserService.java): создан класс UserService

---
+++ src/main/java/com/example/filmorate/service/UserService.java
@@ -0,0 +1,62 @@
+package com.example.filmorate.service;
+
+import com.example.filmorate.exeption.EntityNotFoundException;
+import com.example.filmorate.model.User;
+import com.example.filmorate.storage.user.UserStorage;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class UserService {
+    private final UserStorage userStorage;
+
+    public UserService(UserStorage userStorage) {
+        this.userStorage = userStorage;
+    }
+
+    // Добавление в друзья (взаимное)
+    public void addFriend(Long userId, Long friendId) {
+        User user = userStorage.getUserById(userId);
+        User friend = userStorage.getUserById(friendId);
+
+        if (!user.getFriends().contains(friendId)) {
+            user.getFriends().add(friendId);
+            friend.getFriends().add(userId);
+        }
+    }
+
+    // Удаление из друзей (взаимное)
+    public void removeFriend(Long userId, Long friendId) {
+        User user = userStorage.getUserById(userId);
+        User friend = userStorage.getUserById(friendId);
+
+        if (!user.getFriends().contains(friendId)) {
+            user.getFriends().remove(friendId);
+            friend.getFriends().remove(userId);
+        }
+    }
+
+    // Получение списка друзей пользователя
+    public List<User> getFriends(Long userId) {
+        User user = userStorage.getUserById(userId);
+
+        return user.getFriends().stream()
+                .map(userStorage::getUserById)
+                .collect(Collectors.toList());
+    }
+
+    // Получение списка общих друзей
+    public List<User> getCommonFriends(Long userId, Long otherUserId) {
+        User user = userStorage.getUserById(userId);
+        User otherUser = userStorage.getUserById(otherUserId);
+
+        Set<Long> commonFriendsIds = new HashSet<>(user.getFriends());
+        commonFriendsIds.retainAll(otherUser.getFriends());
+
+        return commonFriendsIds.stream()
+                .map(userStorage::getUserById)
+                .collect(Collectors.toList());
+    }
+}

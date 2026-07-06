e160d9feb3992057fa4ce2d3db2b66d62f736682
igor-gordeev-24-7 <igor.gordeev.24.7@gmail.com>
2026-07-06 17:29:11 +0300

    refactor (UserController.java): добавлены методы для работы с друзьями

--- src/main/java/com/example/filmorate/controller/UserController.java
+++ src/main/java/com/example/filmorate/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.filmorate.controller;
 
 import com.example.filmorate.model.User;
+import com.example.filmorate.service.UserService;
 import com.example.filmorate.storage.user.InMemoryUserStorage;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -8,15 +9,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
     private final InMemoryUserStorage inMemoryUserStorage;
+    private final UserService userService;
 
-    public UserController(InMemoryUserStorage inMemoryUserStorage) {
+    public UserController(InMemoryUserStorage inMemoryUserStorage, UserService userService) {
         this.inMemoryUserStorage = inMemoryUserStorage;
+        this.userService = userService;
     }
 
     @PostMapping
@@ -52,4 +56,26 @@ public class UserController {
     public User deleteUserById(@PathVariable Long id) {
         return inMemoryUserStorage.deleteUserById(id);
     }
-}
+
+    @PutMapping("/{id}/friends/{friendId}")
+    public void addFriend (@PathVariable Long id, @PathVariable Long friendId) {
+        userService.addFriend(id, friendId);
+    }
+
+    @DeleteMapping("/{id}/friends/{friendId}")
+    public void removeFriend(@PathVariable Long id, @PathVariable Long friendId) {
+        userService.removeFriend(id, friendId);
+    }
+
+    @GetMapping("{id}/friends")
+    public List<User> getFriend(@PathVariable Long id) {
+        return userService.getFriends(id);
+    }
+
+    @GetMapping("/{id}/friends/common/{otherId}")
+    public List<User> getCommonFriends(@PathVariable Long id, @PathVariable Long otherId) {
+        return userService.getCommonFriends(id, otherId);
+    }
+ }
+
+ 

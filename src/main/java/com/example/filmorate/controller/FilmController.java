c1b67e846cbf2df20f20a9699dd41c7da3579992
igor-gordeev-24-7 <igor.gordeev.24.7@gmail.com>
2026-07-12 12:56:42 +0300

    refactor (FilmController.java): убрал InMemoryFilmStorage, использовал FilmService

--- src/main/java/com/example/filmorate/controller/FilmController.java
+++ src/main/java/com/example/filmorate/controller/FilmController.java
@@ -2,9 +2,7 @@ package com.example.filmorate.controller;
 
 import com.example.filmorate.model.Film;
 import com.example.filmorate.service.FilmService;
-import com.example.filmorate.storage.film.InMemoryFilmStorage;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,41 +15,42 @@ import java.util.Map;
 @RequestMapping("/films")
 public class FilmController {
 
-    @Autowired
-    InMemoryFilmStorage inMemoryFilmStorage;
-    @Autowired
-    FilmService filmService;
+    public final FilmService filmService;
+
+    public FilmController(FilmService filmService) {
+        this.filmService = filmService;
+    }
 
     @PostMapping
     public ResponseEntity<Film> addFilm(@Valid @RequestBody Film film) {
-        Film savedFilm = inMemoryFilmStorage.addFilm(film);
+        Film savedFilm = filmService.addFilm(film);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedFilm);
     }
 
     @GetMapping
     public Collection<Film> getAllFilms() {
-        return inMemoryFilmStorage.getAllFilms();
+        return filmService.getAllFilms();
     }
 
     @GetMapping("/{id}")
     public Film getFilmById(@PathVariable Long id) {
-        return inMemoryFilmStorage.getFilmById(id);
+        return filmService.getFilmById(id);
     }
 
     @PutMapping()
     public Film updateFilm(@Valid @RequestBody Film film) {
-        return inMemoryFilmStorage.updateFilm(film.getId(), film);
+        return filmService.updateFilm(film.getId(), film);
     }
 
     @DeleteMapping()
     public ResponseEntity<Map<String, String>> deleteFilms() {
-        inMemoryFilmStorage.deleteFilms();
+        filmService.deleteFilms();
         return ResponseEntity.ok(Map.of("message", "Все записи успешно удалены"));
     }
 
     @DeleteMapping("/{id}")
     public Film deleteFilmById(@PathVariable Long id) {
-        return inMemoryFilmStorage.deleteFilmById(id);
+        return filmService.deleteFilmById(id);
     }
 
     @PutMapping("/{id}/like/{userId}")

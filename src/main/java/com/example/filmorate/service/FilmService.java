524616fd288c1ec6d9ded962e9d05f63a90d1b93
igor-gordeev-24-7 <igor.gordeev.24.7@gmail.com>
2026-07-12 13:31:31 +0300

    refactor (FilmService.java): перенес обработку исключений в Service из Storage

--- src/main/java/com/example/filmorate/service/FilmService.java
+++ src/main/java/com/example/filmorate/service/FilmService.java
@@ -25,15 +25,33 @@ public class FilmService {
     }
 
     public List<Film> getAllFilms() {
-        return filmStorage.getAllFilms();
+        List<Film> films = filmStorage.getAllFilms();
+        if (films.isEmpty()) {
+            throw new EntityNotFoundException(
+                    "Фильмы не найдены",
+                    List.of("Список фильмов пуст"));
+        }
+        return films;
     }
 
     public Film getFilmById(Long id) {
-        return filmStorage.getFilmById(id);
+        Film film = filmStorage.getFilmById(id);
+        if (film == null) {
+            throw new EntityNotFoundException(
+                    "Фильм с id " + id + " не найден",
+                    List.of("Проверьте корректный ли id"));
+        }
+        return film;
     }
 
     public Film updateFilm(Long id, Film film) {
-        return filmStorage.updateFilm(id, film);
+        Film updatedFilm = filmStorage.updateFilm(id, film);
+        if (updatedFilm == null) {
+            throw new EntityNotFoundException(
+                    "Фильм с id " + id + " не найден",
+                    List.of("Проверьте корректный ли id"));
+        }
+        return updatedFilm;
     }
 
     public void deleteFilms() {
@@ -41,13 +59,18 @@ public class FilmService {
     }
 
     public Film deleteFilmById(Long id) {
-        return filmStorage.deleteFilmById(id);
+        Film deletedFilm = filmStorage.deleteFilmById(id);
+        if (deletedFilm == null) {
+            throw new EntityNotFoundException(
+                    "Фильм с id " + id + " не найден",
+                    List.of("Проверьте корректный ли id"));
+        }
+        return deletedFilm;
     }
 
     // Добавление лайка
     public void addLike(Long filmId, Long userId) {
-        Film film = filmStorage.getFilmById(filmId);
-
+        Film film = getFilmById(filmId);
         User user = userStorage.getUserById(userId);
 
         if (film.getLikes() == null) {
@@ -61,8 +84,7 @@ public class FilmService {
 
     // Удаление лайка
     public void removeLike(Long filmId, Long userId) {
-        Film film = filmStorage.getFilmById(filmId);
-
+        Film film = getFilmById(filmId);
         User user = userStorage.getUserById(userId);
 
         if (film.getLikes() == null) {
@@ -80,7 +102,9 @@ public class FilmService {
 
     // Получение топ-N популярных фильмов по количеству лайков
     public List<Film> getTopPopularFilms(int count) {
-        return filmStorage.getAllFilms().stream()
+        List<Film> films = getAllFilms();
+
+        return films.stream()
                 .map(film -> {
                     if (film.getLikes() == null) {
                         film.setLikes(new HashSet<>());
@@ -94,5 +118,4 @@ public class FilmService {
                 .limit(count)
                 .collect(Collectors.toList());
     }
-
 }
